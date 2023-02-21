eaa0e9f9f2b65c24e4b7e5bfce71106c04e3871f
Mike Wright <thespoonftw@gmail.com>
2023-02-21 12:21:15 +0000

    update test upload page

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -6,15 +6,18 @@ export class Upload extends Component {
   render() {
     return (
       <div>
-        <h4>Upload photo: </h4>
+        <h4>Post photo: </h4>
         <span> Url: </span><input id="photo-url-input" type="text"></input>
         <span> Channel ID: </span><input id="photo-channelId-input" type="text"></input>
+        <span> Uploader ID: </span><input id="photo-uploaderId-input" type="text"></input>
+        <span> Caption: </span><input id="photo-caption-input" type="text"></input>
         <button onClick={() => this.postPhoto()}>Send</button>
         <br />
         <br />
-        <h4> Create album: </h4>
-        <span>Name: </span><input id="album-name-input" type="text"></input>
+        <h4> Post album: </h4>
+        <span> Name: </span><input id="album-name-input" type="text"></input>
         <span> Channel ID: </span><input id="album-channelId-input" type="text"></input>
+        <span> Participant IDs: </span><input id="album-participantIds-input" type="text"></input>
         <button onClick={() => this.postAlbum()}>Send</button>
       </div>      
     );
@@ -22,12 +25,12 @@ export class Upload extends Component {
 
   postPhoto() {
 
-    const url = document.getElementById("photo-url-input").value;
-    const channelId = document.getElementById("photo-channelId-input").value;
-
     const photoPost = {
-      "url": url,
-      "channelId": channelId
+      "url": document.getElementById("photo-url-input").value,
+      "channelId": document.getElementById("photo-channelId-input").value,
+      "uploaderId": document.getElementById("photo-uploaderId-input").valuerId,
+      "uploadTime": new Date().toISOString(),
+      "caption": document.getElementById("photo-caption-input").value
     }
 
     fetch('photo', {
@@ -41,12 +44,10 @@ export class Upload extends Component {
 
   postAlbum() {
 
-    const name = document.getElementById("album-name-input").value;
-    const channelId = document.getElementById("album-channelId-input").value;
-
     const albumPost = {
-      "name": name,
-      "channelId": channelId
+      "name": document.getElementById("album-name-input").value,
+      "channelId": document.getElementById("album-channelId-input").value,
+      "participantIds": document.getElementById("album-participantIds-input").value.split(',').filter(i => i)
     }
 
     fetch('album', {
