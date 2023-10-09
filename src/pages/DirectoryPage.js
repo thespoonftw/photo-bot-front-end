cf9b78ed6f9e0fb98c9b22e3d3e016ce535bbfe5
Mike Wright <thespoonftw@gmail.com>
2023-10-09 13:56:27 +0100

    sort albums newest to oldest, remove mini albums

--- src/pages/DirectoryPage.js
+++ src/pages/DirectoryPage.js
@@ -18,14 +18,20 @@ export class DirectoryPage extends Component {
 
     if (!User.getUser()) { return; }
 
-    const albums = await Http.getAlbumList();    
-    const featured = albums.filter(a => a.numberOfPhotos >= 10);
-    const mini = albums.filter(a => a.numberOfPhotos < 10);
+    const albums = await Http.getAlbumList();
 
-    const f_years = Array.from(new Set(featured.map(a => a.year))).filter(y => y !== 0);
-    const m_years = Array.from(new Set(mini.map(a => a.year))).filter(y => y !== 0);
+    // sort newest to oldest
+    albums.sort((a, b) => {
+      if (a.year === b.year) {
+        return b.month - a.month;
+      }
+      return b.year - a.year;
+    });
+
+    const years = Array.from(new Set(albums.map(a => a.year))).filter(y => y !== 0);
     
-    this.setState({ loaded: true, featured: featured, mini: mini, f_years: f_years, m_years: m_years });
+    
+    this.setState({ loaded: true, albums: albums, years: years });
   }
 
   render () {
@@ -37,29 +43,17 @@ export class DirectoryPage extends Component {
             <h3>Albums</h3>
             <Vert height='1'></Vert>
             
-            { this.state.f_years.map(y =>
+            { this.state.years.map(y =>
               <>
                 <p><b>{y}</b></p>
-                { this.state.featured.filter(a => a.year === y).map(a => 
+                { this.state.albums.filter(a => a.year === y).map(a => 
                   <div key={a.id}><Link to={"/album/" + a.id} style={{fontSize: "20px"}} ><AlbumTag name={a.name}/></Link></div>
                 )}
                 <Vert />
               </>
             )}          
             
-            <Vert height='5'></Vert>
-            <h3>Mini Albums</h3>
-            <Vert height='1'></Vert>
-
-            { this.state.m_years.map(y =>
-              <>
-                <p><b>{y}</b></p>
-                { this.state.mini.filter(a => a.year === y).map(a => 
-                  <div key={a.id}><Link to={"/album/" + a.id} style={{fontSize: "20px"}} ><AlbumTag name={a.name}/></Link></div>
-                )}
-                <Vert />
-              </>
-            )}
+            <Vert height='1'></Vert>            
           </>)
           :
           <p>You must be logged in to browse albums.</p>
